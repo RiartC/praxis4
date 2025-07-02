8a607c517064f6532f77290b222474237d240555
RiartC <162469575+RiartC@users.noreply.github.com>
2025-07-02 07:57:57 +0200

    Create MenuController.java

---
+++ MenuController.java
@@ -0,0 +1,11 @@
+@RestController
+@RequestMapping("/api/menu")
+public class MenuController {
+    @Autowired
+    private MenuService service;
+
+    @GetMapping
+    public List<MenuItem> getMenu() {
+        return service.getAllMenuItems();
+    }
+}
